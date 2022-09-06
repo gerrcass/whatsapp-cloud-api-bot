5edb7dd41f7e161b91b0fb68d1069433ebf32675
Gerardo Castillo <gerrcass@gmail.com>
2022-09-05 23:57:43 -0400

    test POST endpoint

--- src/routes/messaging-webhook/+server.js
+++ src/routes/messaging-webhook/+server.js
@@ -3,12 +3,12 @@ import { v4 as uuid } from '@lukeed/uuid';
 
 /** @type {import('./$types').RequestHandler} */
 
+const kvLogId = uuid()
+
 export const GET = async ({ url, platform, request }) => {
 
-    const kvlogId = uuid()
-
     const timezone = request.cf && request.cf.timezone;
-    let localized_date = new Date(new Date().toLocaleString('en-US', { timeZone: timezone }));
+    const localized_date = new Date(new Date().toLocaleString('en-US', { timeZone: timezone }));
 
     const mode = url.searchParams.get('hub.mode') || null
     const token = url.searchParams.get('hub.verify_token') || null
@@ -22,14 +22,28 @@ export const GET = async ({ url, platform, request }) => {
         if (mode === "subscribe" && token === verifyToken) {
             // Respond with the challenge token from the request
             //console.log("WEBHOOK_VERIFIED");
-            platform.env.LOGS && await platform.env.LOGS.put(kvlogId, `WEBHOOK_VERIFIED | ${localized_date.toGMTString()} | mode: ${mode} token: ${token} challenge: ${challenge}`)
+            platform.env.LOGS && await platform.env.LOGS.put(`GET_${kvLogId}`, `WEBHOOK_VERIFIED | ${localized_date.toGMTString()} | mode: ${mode} token: ${token} challenge: ${challenge}`)
             return new Response(String(challenge));
             //res.status(200).send(challenge);
         } else {
             // Respond with '403 Forbidden' if verify tokens do not match
             //res.sendStatus(403);
+
+            platform.env.LOGS && await platform.env.LOGS.put(`GET_ERROR_${kvLogId}`, `TOKEN_DO_NOT_MATCH | ${localized_date.toGMTString()} | mode: ${mode} token: ${token} challenge: ${challenge}`)
             throw error(403);
         }
     }
     return new Response(String("Nothing from Facebook"));
+}
+
+export const POST = async ({ platform, request }) => {
+    try {
+        const requestBody = await request.json()
+        //console.log('POST: Someone is pinging me!');
+        platform.env.LOGS && await platform.env.LOGS.put(`POST_${kvLogId}`, JSON.stringify(requestBody))
+        return new Response(String("OK"));
+    } catch (error) {
+        platform.env.LOGS && await platform.env.LOGS.put(`POST_ERROR_${kvLogId}`, JSON.stringify(error))
+        throw error(500);
+    }
 }
